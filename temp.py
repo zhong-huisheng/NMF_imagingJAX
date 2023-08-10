f828dd4a326f1404f57163a9905584f1aedf07ed
ivanabreu0 <102328381+ivanabreu0@users.noreply.github.com>
2023-08-10 12:33:46 -0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -11,15 +11,15 @@ import time
 #import jax.numpy as jpn
 #from jax import grad
 #t0 = time.time()
-a, ref, trg, angles, coords, z = [], [], [], [], [], []
-cond, counter2, counter = True, 0, 0
+a, ref, angles, coords, = [], [], [], []
+cond, counter2 = True, 0
 
 trg_bool = True
 directory = os.fsencode("/home/ivanabreu/test_work/individual_exposures_calib-2")
   #Put in path to references
 
-ra1, dec1 = (344.4110073, -29.6196750)
-ra2, dec2 = (344.4118405, -29.6203386)
+ra1, dec1 = (344.4110073, -29.6186750)
+ra2, dec2 = (344.4118405, -29.6206386)
 masks_fom = np.zeros((12,1100,1100))
 for x in sorted(os.listdir(directory)):
     temp_file = fits.open(os.path.join(directory, x))
@@ -27,8 +27,7 @@ for x in sorted(os.listdir(directory)):
         header = WCS(temp_file[0].header)
         cond = False
     if counter2 == 0:
-        data_min = np.median(temp_file[0].data)
-        trg.append(temp_file[0].data - data_min)
+        a.append(temp_file[0].data)
         w = WCS(temp_file[0].header)
         x1, y1 = w.all_world2pix(ra1, dec1, 1)
         x2, y2 = w.all_world2pix(ra2, dec2, 1)
@@ -38,25 +37,19 @@ for x in sorted(os.listdir(directory)):
         coords.append([temp_file[0].header['CRPIX1'], temp_file[0].header['CRPIX2']])
     else:
         a.append(temp_file[0].data)
-        z.append(temp_file[0].data)
     counter2 += 1
     if counter2 == 28:
         counter2 = 0
-        counter += 1
         ref_min = np.median(np.nanmedian(a, 0))
         ref.append(np.nanmedian(a, 0) - ref_min)
         a.clear()
 
     temp_file.close()
 
-z = np.array(z)
-trg = np.array(trg)
 ref = np.array(ref)
-a = np.array(a)
 masks_fom[masks_fom < 0.9] = 0.1
 
 
-trg[trg<=0] = 1e-6
 ref[ref<=0] = 1e-6
 
 
@@ -72,37 +65,36 @@ mask[mask >= -1] = 1.
 mask[mask < -1] = 0.1 #mask cleanup
 
 
-b = np.ones((11,1100,1100))
-trg_errs = np.ones(trg.shape)
+refshape = (ref.shape[0]-1, ref.shape[1], ref.shape[2])
+ref_err = np.ones(refshape)
+trg_errs = np.ones(ref.shape)
 componentNum = 11
 #components = nmf_imaging.NMFcomponents(ref, ref_err=b, mask = mask, n_components = componentNum, oneByOne=True)
-results = np.zeros(trg.shape) # Say trgs is a 3D array containing the targets that need NMF modeling, then results store the NMF subtraction results.
+results = np.zeros(ref.shape) # Say trgs is a 3D array containing the targets that need NMF modeling, then results store the NMF subtraction results.
 maxiters = 1e5
 
 
 t0 = time.time()
-for i in range(trg.shape[0]):
+for i in range(ref.shape[0]):
       #can replace component, model, and result lines with commented lines below it to process whole image instead of doing data imputation
     ref_new = copy.deepcopy(ref)
     ref_new = np.delete(ref_new, i, 0)
-    print(ref_new.shape)
-    components = nmf_imaging.NMFcomponents(ref_new, ref_err=b, mask = masks_fom[i], n_components = componentNum, oneByOne=True, maxiters=maxiters)
-    #components = nmf_imaging.NMFcomponents(ref_new, ref_err=b, mask = mask, n_components = componentNum, oneByOne=True, maxiters=maxiters)
-  
-    trgs = trg[i]
-    print(i)
+    components = nmf_imaging.NMFcomponents(ref_new, ref_err=ref_err, mask = masks_fom[i], n_components = componentNum, oneByOne=True, maxiters=maxiters)
+    #components = nmf_imaging.NMFcomponents(ref_new, ref_err=ref_err, mask = mask, n_components = componentNum, oneByOne=True, maxiters=maxiters)
+    trgs = ref[i]
     trg_err = trg_errs[i]
     model = nmf_imaging.NMFmodelling(trg = trgs, trg_err=trg_err, components = components, n_components = componentNum, mask_components=mask, trgThresh=0.0, maxiters=maxiters, mask_data_imputation=masks_fom[i]) # Model the target with the constructed components.
     #model = nmf_imaging.NMFmodelling(trg = trgs, trg_err=trg_err, components = components, n_components = componentNum, mask_components=mask, trgThresh=0.0, maxiters=maxiters)
     best_frac = 1.
     result = nmf_imaging.NMFsubtraction(trgs, model, masks_fom[i], frac = best_frac) # Subtract the best model from the target
     #result = nmf_imaging.NMFsubtraction(trgs, model, mask, frac = best_frac)
+    print(str(i)+" target calculated")
     results[i] = result
     # Now `results' stores the NMF subtraction results of the targets.
 
 
 results_new = []
-for i in range(trg.shape[0]):
+for i in range(ref.shape[0]):
     if i == 0:
         results_new.append(pyklip.rotate(results[i], angles[i], coords[i], astr_hdr=header))
     else:
